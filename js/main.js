31c885f16abfa2a45e9b992961517d946c5d2f2b
Grimreaper <sakthimanimchss@gmail.com>
2025-12-27 00:26:12 +0530

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("menu");
-
-hamburger.onclick = () => {
-  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
-};
